8bc3dc77e03372db388a47ce3bd3c3390de47dad
Molly Gilbert <msgilbert389@gmail.com>
2020-07-16 15:41:56 -0500

    deleting code

--- portfolio/js/script.js
+++ portfolio/js/script.js
@@ -275,28 +275,6 @@
         event.preventDefault();
     });
 
-    //google map load
-    // $('#map_canvas').gmap({
-    //     'center': '-6.94010,107.62575',
-    //     'zoom': 15,
-    //     scrollwheel: false,
-    //     'disableDefaultUI': false,
-    //     'styles': [{
-    //         stylers: [{
-    //             lightness: 7
-    //         }, {
-    //             saturation: -100
-    //         }]
-    //     }],
-    //     'callback': function() {
-    //         var self = this;
-    //         self.addMarker({
-    //                 'position': this.get('map').getCenter(),
-    //                 icon: 'images/office-building.png',
-    //             })
-    //     }
-    // });
-
     //typed jquery
     var typed = new Typed('.personal-color', {
         strings: ["Create", "Imagine", "Code", "Work", ],
